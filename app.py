d2bb4c595e1012416c16a2b49379b4f142666d7d
jason <jasonfchan@gmail.com>
2020-05-22 11:50:16 -0700

    create module to ingest and turn csv to dataframe. add requirements.text

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import csv_utils
 
-record_date = "04-12-2020"
+record_date = "05-12-2020"
+url_path = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
 
-us_state_df = csv_utils.csv_to_df("04-12-2021")
+all_country_df = csv_utils.ingest_coviddata(record_date, url_path)
+print(all_country_df.columns)
+
+us_state_df = csv_utils.csv_to_us_df(record_date)
+
+print(us_state_df.head(5))
--- csv_utils.py
+++ csv_utils.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import requests
 
+""""""
+def ingest_coviddata(date_str : str, path : str):
+    """Downloads global covid19 daily data with date and path string. Returns a dataframe of csv"""
 
-def ingest_coviddata(date_str : str):
-    url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date_str}.csv"
+    url = f"{path}{date_str}.csv"
     try:
         r = requests.get(url, allow_redirects=True)
         open(f'data/{date_str}.csv', 'wb').write(r.content)
+        return pd.read_csv(f'data/{date_str}.csv', header=0)
     except requests.exceptions.RequestException as e:
         print(f"Requests Exceptions {e}")
 
-def csv_to_df(date_str : str):
 
+
+def csv_to_us_df(date_str : str):
+    """Function filters covid19 dataframe to US only using date. Returns a dataframe"""
     try:
         df = pd.read_csv(f'data/{date_str}.csv', header=0)
         us_df = df[(df.Country_Region == 'US')]
-        print(us_df.head(5))
-        print(us_df.columns)
         us_state_df = us_df.groupby('Province_State').agg({'Deaths': 'sum',})
-        print(us_state_df)
         return us_state_df
     except OSError as e:
         print(f"{e}")
